42846ad146a675cc9a582cee2fff779ce32771ec
Gopi_krishnan01 <gopi.krishnan@mindcurv.com>
2023-09-04 10:46:31 +0530

    removed the spaces

--- cypress/e2e/common.cy.js
+++ cypress/e2e/common.cy.js
@@ -1,12 +1,6 @@
 describe("To automate the facebook login", () => {
-
     it("Facebook username", () => {
         cy.visit('www.facebook.com')
-        //cy.get('_sv4').eq(6).click()
         cy.get('.inputtext._55r1._6luy').eq(0).type('gopikrishnan')
     })
-
-
-
-
 })
